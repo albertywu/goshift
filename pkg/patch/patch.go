970704ad0fba5afcc257b486ce0fb6c343b2fbdd
Albert Wu <wua@uber.com>
2023-04-26 22:24:22 -0700

    adds patch package and tests

--- pkg/patch/patch.go
+++ pkg/patch/patch.go
@@ -1,7 +1,12 @@
 package patch
 
-// Patch is a collection of FileDiffs that represent changes made to multiple files.
+import (
+	"os"
 
+	"github.com/pmezard/go-difflib/difflib"
+)
+
+// Patch is a collection of FileDiffs that represent changes made to multiple files.
 type Patch struct {
 	FileDiffs []FileDiff
 }
@@ -16,18 +21,40 @@ type FileDiff struct {
 
 // AddFileDiff appends a FileDiff to a Patch's collection of FileDiffs.
 func (p *Patch) AddFileDiff(fileDiff FileDiff) {
-	// ...
+	p.FileDiffs = append(p.FileDiffs, fileDiff)
 }
 
 // WriteToFile writes a Patch's collection of FileDiffs to a file at the specified output path.
 func (p *Patch) WriteToFile(outputPath string) error {
-	// ...
+	file, err := os.Create(outputPath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	for _, fileDiff := range p.FileDiffs {
+		_, err = file.WriteString(fileDiff.Diff)
+		if err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
 
 // GenerateDiff computes the diff between the original and modified text,
 // returning the diff as a string or an error if the diff cannot be generated.
-func GenerateDiff(original, modified string) (string, error) {
-	// ...
-	return "", nil
+func GenerateDiff(originalPath, original, modifiedPath, modified string) (string, error) {
+	diff := difflib.UnifiedDiff{
+		A:        difflib.SplitLines(original),
+		B:        difflib.SplitLines(modified),
+		FromFile: originalPath,
+		ToFile:   modifiedPath,
+		Context:  3,
+	}
+	result, err := difflib.GetUnifiedDiffString(diff)
+	if err != nil {
+		return "", err
+	}
+	return result, nil
 }
--- pkg/patch/patch_test.go
+++ pkg/patch/patch_test.go
@@ -1,17 +1,93 @@
 package patch_test
 
 import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
 	"testing"
+
+	"github.com/albertywu/goshift/pkg/patch"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func TestAddFileDiff(t *testing.T) {
-	// ...
-}
+func TestPatch_WriteToFile(t *testing.T) {
+	tempDir, err := ioutil.TempDir("", "patchtest")
+	require.NoError(t, err)
+	defer os.RemoveAll(tempDir)
 
-func TestWriteToFile(t *testing.T) {
-	// ...
+	outputPath := filepath.Join(tempDir, "patchfile.diff")
+
+	p := patch.Patch{}
+	fileDiff := patch.FileDiff{
+		OriginalPath: "original.go",
+		ModifiedPath: "modified.go",
+		Diff: `--- original.go
++++ modified.go
+@@ -1,5 +1,5 @@
+ package main
+
+-func oldName() {
++func newName() {
+ 	println("Hello, world!")
+ }
+`,
+	}
+	p.AddFileDiff(fileDiff)
+
+	err = p.WriteToFile(outputPath)
+	require.NoError(t, err)
+
+	content, err := os.ReadFile(outputPath)
+	require.NoError(t, err)
+
+	assert.Equal(t, fileDiff.Diff, string(content), "Expected written diff to match the added FileDiff")
 }
 
 func TestGenerateDiff(t *testing.T) {
-	// ...
+	original := `package main
+
+func oldName() {
+	println("Hello, world!")
+}
+
+func main() {
+	oldName()
+}
+`
+
+	modified := `package main
+
+func newName() {
+	println("Hello, world!")
+}
+
+func main() {
+	newName()
+}
+`
+
+	expectedDiff := `--- original.go
++++ modified.go
+@@ -1,10 +1,10 @@
+ package main
+ 
+-func oldName() {
++func newName() {
+ 	println("Hello, world!")
+ }
+ 
+ func main() {
+-	oldName()
++	newName()
+ }
+`
+
+	originalPath := "original.go"
+	modifiedPath := "modified.go"
+
+	diff, err := patch.GenerateDiff(originalPath, original, modifiedPath, modified)
+	require.NoError(t, err)
+	assert.Equal(t, strings.TrimSpace(expectedDiff), strings.TrimSpace(diff), "Expected generated diff to match the expected diff")
 }
