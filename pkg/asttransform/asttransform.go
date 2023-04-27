d3cb21f8c1c381f41d4f5a6d50bcff5db5adf752
Albert Wu <wua@uber.com>
2023-04-26 22:10:53 -0700

    adds asttransform and tests

--- pkg/asttransform/asttransform.go
+++ pkg/asttransform/asttransform.go
@@ -1,6 +1,7 @@
 package asttransform
 
 import (
+	"fmt"
 	"go/ast"
 )
 
@@ -11,6 +12,17 @@ type TransformFunc func(node ast.Node) (ast.Node, error)
 // ApplyTransforms applies a series of TransformFuncs to an ast.Node,
 // returning the modified ast.Node or an error if any of the transformations fail.
 func ApplyTransforms(node ast.Node, transforms []TransformFunc) (ast.Node, error) {
-	// ...
-	return nil, nil
+	if node == nil {
+		return nil, fmt.Errorf("node is nil")
+	}
+
+	var err error
+	for _, transform := range transforms {
+		node, err = transform(node)
+		if err != nil {
+			return nil, fmt.Errorf("error applying transform: %w", err)
+		}
+	}
+
+	return node, nil
 }
--- pkg/asttransform/asttransform_test.go
+++ pkg/asttransform/asttransform_test.go
@@ -1,1 +1,67 @@
 package asttransform_test
+
+import (
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"testing"
+
+	"github.com/albertywu/goshift/pkg/asttransform"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+// sampleTransform is a simple transform function that renames all functions
+// with the name "oldName" to "newName".
+func sampleTransform(oldName, newName string) asttransform.TransformFunc {
+	return func(node ast.Node) (ast.Node, error) {
+		ast.Inspect(node, func(n ast.Node) bool {
+			fn, ok := n.(*ast.FuncDecl)
+			if ok && fn.Name.Name == oldName {
+				fn.Name.Name = newName
+			}
+			return true
+		})
+		return node, nil
+	}
+}
+
+func TestApplyTransforms(t *testing.T) {
+	src := `
+package main
+
+func oldName() {
+	println("Hello, world!")
+}
+
+func main() {
+	oldName()
+}
+`
+
+	fset := token.NewFileSet()
+	file, err := parser.ParseFile(fset, "", src, 0)
+	require.NoError(t, err)
+
+	transforms := []asttransform.TransformFunc{
+		sampleTransform("oldName", "newName"),
+	}
+
+	modifiedNode, err := asttransform.ApplyTransforms(file, transforms)
+	require.NoError(t, err)
+
+	modifiedFile, ok := modifiedNode.(*ast.File)
+	require.True(t, ok, "Expected modifiedNode to be of type *ast.File")
+
+	// Check if the function was renamed
+	found := false
+	for _, decl := range modifiedFile.Decls {
+		fn, ok := decl.(*ast.FuncDecl)
+		if ok && fn.Name.Name == "newName" {
+			found = true
+			break
+		}
+	}
+
+	assert.True(t, found, "Expected to find a function with the name 'newName'")
+}
